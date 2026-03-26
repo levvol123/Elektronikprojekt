ae23d61550010e3712a253f7af971fb722103d30
Mutanton <an2205ni-s@student.lu.se>
2026-03-26 12:53:30 +0100

    ljkjgfdfs

--- actualprojekt/Signalprocessing.c
+++ actualprojekt/Signalprocessing.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    printf("test\n");
-    return 0;
+
+float calculate_delta_time() {
+    
 }
 
-int detect_peak(int* data) {
-    return 0;
-}
 
-float calculate_angle(float delta_time) {
+double calculate_angle(float delta_time) {
     float c = 340; //ljudhastighet
     float d = 0.01; //avstånd mellan mikrofoner
-    return asin(c*delta_time/d);
+    return asin((double) (c*delta_time)/d)*180/3.1415926;
+}
+
+
+int main() {
+    float t1 = 0.00001;
+    float t2 = 0.00003;
+    float d_t = t2 - t1;
+    printf("%lf\n", calculate_angle(d_t));
 }
