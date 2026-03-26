0705d63fb0499b46cf29917dad1882262b7de55b
Mutanton <an2205ni-s@student.lu.se>
2026-03-26 10:51:54 +0100

    skapat filer

--- actualprojekt/Signalprocessing.c
+++ actualprojekt/Signalprocessing.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     printf("test\n");
     return 0;
+}
+
+int detect_peak(int* data) {
+    return 0;
+}
+
+float calculate_angle(float delta_time) {
+    float c = 340; //ljudhastighet
+    float d = 0.01; //avstånd mellan mikrofoner
+    return asin(c*delta_time/d);
 }
