ac67d25d16067e4df354db6d6d0bddf62d4ebdbc
Mutanton <an2205ni-s@student.lu.se>
2026-03-29 12:57:19 +0200

    tank

--- actualprojekt/Signalprocessing.c
+++ actualprojekt/Signalprocessing.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+int max(int* v, int length) {
+    int max_value = v[0];
+    for (int i = 1; i < length; i++) {
+        if (v[i] > max_value) {
+            max_value = v[i];
+        }
+    }
+    return max_value;
+}
+
 int* padd_zero(int* v, int length) {
     for (int i=(length-1); i < 0; i--){
         if (i=0){
@@ -13,41 +23,44 @@ int* padd_zero(int* v, int length) {
 }
 
 
+
 float cross_correlation(int* v1, int* v2, int length) {
-    float max_delta_time = 0; //ÄNDRA!!!!
-    float t_sample = 0;
+    float max_delta_time = 4; 
+    float t_sample = 1;
     int max_padding = (int) max_delta_time/t_sample;
-    int best_index;
+    int best_padding = 0;
     int best_sum = 0;
-    int m_v1 = max(v1);
-    int m_v2 = max(v2);
+    int m_v1 = max(v1, length);
+    int m_v2 = max(v2, length);
     if (m_v1 <= m_v2) {
+        printf("v1 är mindre än v2");
         for (int current_padding = 0; current_padding < max_padding; current_padding++) {
             int sum = 0;
             for (int i = 0; i < length-1; i++) {
                 sum += v1[i]*v2[i];
             }
             if (sum > best_sum) {
-                best_index = current_padding;
+                best_padding = current_padding;
                 best_sum = sum;
             }
             padd_zero(v2, length);
         }
     } else {
+        printf("v2 är mindre än v1");
         for (int current_padding = 0; current_padding < max_padding; current_padding++) {
             int sum = 0;
             for (int i = 0; i < length-1; i++) {
                 sum += v1[i]*v2[i];
             }
             if (sum > best_sum) {
-                best_index = current_padding;
+                best_padding = current_padding;
                 best_sum = sum;
             }
             padd_zero(v1, length);
         }
     }
-
-
+    float delta_time = best_padding * t_sample;
+    return delta_time;
 }
 
 double calculate_angle(float delta_time) {
@@ -57,7 +70,7 @@ double calculate_angle(float delta_time) {
 }
 
 int main() {
-    int* vector1 = {0, 0, 1, 3, 5, 2, 0};
-    int* vector2 = {2, 4, 6, 2, 0, 0, 0};
-
+    int vector1[7] = {0, 0, 1, 3, 5, 2, 0};
+    int vector2[7] = {2, 4, 6, 2, 0, 0, 0};
+    printf("%f", cross_correlation(vector1, vector2, 7));
 }
