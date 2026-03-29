bf29e1897511cb2be2e16e799a201ab65ef6a2c4
Lev WSL <lev.volkov999@gmail.com>
2026-03-29 14:38:03 +0200

    delvis fixat f_loop, ska implementera buffer nu

--- actualprojekt/Datafetcher.c
+++ actualprojekt/Datafetcher.c
@@ -1,6 +1,9 @@
 #include "Datafetcher.h"
 #include <pthread.h> //https://pubs.opengroup.org/onlinepubs/7908799/xsh/pthread.h.html
 
+static int current_state;
+static int previous_state;
+
 static pthread_t adc_thread;
 
 int f_configure(ADS1263_DRATE samplerate) {
@@ -13,9 +16,18 @@ int f_configure(ADS1263_DRATE samplerate) {
 	return 0;
 }
 void f_start_loop() {
+	current_state = 1;
+	previous_state = 1;
 	pthread_create(&adc_thread, NULL, f_loop, NULL);
 }
 static void* f_loop() {
+	while(1){
+		current_state = DEV_Digital_Read(DEV_DRDY_PIN);
+		if(current_state == 0 && previous_state == 1){
+			//skriv till buffer
+		} 
+		previous_state = current_state;
+	}
 	//fetcha från adc när DRDY = 0
 	//skriv till någon buffer
 	return NULL;
