a94b38aeb1c68f7d2d6719cbb970c28f10603f02
Lev WSL <lev.volkov999@gmail.com>
2026-04-01 14:06:45 +0200

    fixat lite i main.c

--- actualprojekt/Main.c
+++ actualprojekt/Main.c
@@ -1,3 +1,16 @@
+#include "Signalprocessing2.h"
+#include "Datafetcher.h"
+#include "ADS1263.h"
+#include <pthread.h>
+#include <stdio.h>
+
 int main() {
+    printf("Initializing");
+    f_configure(ADS1263_19200SPS);
+    //init singalprocessing2
+    printf("Starting tasks");
+    f_start_loop();
+    //starta signalprocessing2 loop?
+
     return 0;
 }
