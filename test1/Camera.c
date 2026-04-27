c875c966093f067d7afabbc861175bda3091990d
Mutanton <an2205ni-s@student.lu.se>
2026-04-27 11:33:40 +0200

    uppdaterat camera.c

--- test1/Camera.c
+++ test1/Camera.c
@@ -3,11 +3,12 @@
 
 int main()
 {
+	srand(time(NULL));
 	c_setup();
 	c_set_password("root:root");
 	c_set_ip("169.254.45.199");
-	c_pan(0);
-	c_tilt(0);
+	c_pan(rand()% 90);
+	c_tilt(rand()% 90);
 	c_exit();
 	return 0;
 }
