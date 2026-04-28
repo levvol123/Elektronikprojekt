db66c77acd8e174b1a25f92a363df2635dcb8af7
Lev 2 <lev.volkov999@gmail.com>
2026-04-28 14:59:21 +0200

    fixat lite i main

--- actualprojekt/Main.c
+++ actualprojekt/Main.c
@@ -1,12 +1,11 @@
 #include "Signalprocessing2.h"
 #include "Datafetcher.h"
-#include "ADS1263.h"
 #include <pthread.h>
 #include <stdio.h>
 
 int main() {
     printf("Initializing");
-    f_configure(ADS1263_19200SPS);
+    f_configure(100);
     //init singalprocessing2
     printf("Starting tasks");
     f_start_loop();
